6f855511fd6cf76ec7087d2f9b54f764d1ef36ae
ericadiogo <erica.monteiro.diogo@gmail.com>
2024-07-14 16:52:44 -0400

    Added heading to Home view.

--- PomodoroStudies/HomeView.swift
+++ PomodoroStudies/HomeView.swift
@@ -10,19 +10,42 @@ import FirebaseAuth
 
 struct HomeView: View {
     var body: some View {
-        Text("Logged In")
-        
-        Button(action: {
-            let firebaseAuth = Auth.auth()
-            do {
-              try firebaseAuth.signOut()
+        HStack{
+            VStack{
+                Image("PomodoroNinjaSolo-2")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 40, height: 64)
+            }.padding()
+            
+            VStack{
+                Text("Hello, user!")
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    .font(.system(size: 18))
+                    .padding(.bottom,5)
+                    .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/,
+                           alignment: .leading)
                 
-            } catch let signOutError as NSError {
-              print("Error signing out: %@", signOutError)
+                Text("You are level ?")
+                    .font(.system(size: 14))
+                    .padding(.bottom,5)
+                    .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/,
+                           alignment: .leading)
+            }.padding()
+            
+            
+            Button(action: {
+                let firebaseAuth = Auth.auth()
+                do {
+                    try firebaseAuth.signOut()
+                    
+                } catch let signOutError as NSError {
+                    print("Error signing out: %@", signOutError)
+                }
+            }){
+                Image(systemName: "rectangle.portrait.and.arrow.right").padding()
             }
-        }){
-            Text("Sign Out")
-        }
+        }.frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/,maxWidth: .infinity).padding(.leading,20).padding(.trailing,20)
     }
 }
 
