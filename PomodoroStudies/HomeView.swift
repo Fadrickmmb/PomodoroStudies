4be1b0a940d2f9c1d974a2e3814e815170a3b1c5
ericadiogo <erica.monteiro.diogo@gmail.com>
2024-07-22 19:50:48 -0400

    Added profile page design.

--- PomodoroStudies/HomeView.swift
+++ PomodoroStudies/HomeView.swift
@@ -9,7 +9,11 @@ import SwiftUI
 import FirebaseAuth
 
 struct HomeView: View {
-    @State var selectedTab = 0
+    @State private var selectedTab = 0
+    @State private var name = ""
+    @State private var userlevel = ""
+    @State private var username = ""    //email provided by user
+    
     var body: some View {
         
         TabView(selection: $selectedTab) {
@@ -25,14 +29,14 @@ struct HomeView: View {
                     }.padding()
                     
                     VStack{
-                        Text("Hello, user!")
+                        Text("Hello, " + name + "!")
                             .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
                             .font(.system(size: 18))
                             .padding(.bottom,5)
                             .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/,
                                    alignment: .leading)
                         
-                        Text("You are level ?") //add level from database
+                        Text("You are level" + userlevel + ".") //add level from database
                             .font(.system(size: 14))
                             .padding(.bottom,5)
                             .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/,
@@ -62,34 +66,91 @@ struct HomeView: View {
             }.tag(0)
             
             // Profile Tab
-            VStack{
-                HStack{
-                    VStack{
-                        Image("PomodoroNinjaSolo-2")
+            ZStack{
+                Color.red.opacity(1.0).ignoresSafeArea()
+                VStack{
+                    HStack{
+                        Image("BlackMask")
                             .resizable()
                             .scaledToFill()
-                            .frame(width: 40, height: 64)
-                    }.padding()
-                    
-                    VStack{
-                        Text("Hello, user!")
+                            .frame(width: 120,height: 120)
+                    }.padding(40)
+                    VStack(alignment: .leading){
+                        Text("Name")
+                            .foregroundColor(.white)
                             .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
                             .font(.system(size: 18))
-                            .padding(.bottom,5)
-                            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/,
-                                   alignment: .leading)
+                            .padding(.leading,40)
+                            .padding(.trailing,40)
+                            .padding(.bottom,10)
                         
-                        Text("You are level ?")
-                            .font(.system(size: 14))
-                            .padding(.bottom,5)
-                            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/,
-                                   alignment: .leading)
-                    }.padding()
-                }.frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/,maxWidth: .infinity)
-                    .padding(.leading,20)
-                    .padding(.trailing,20)
-                
-                Text("Profile")
+                        TextField("", text: $name, prompt: Text("user's name")
+                            .foregroundColor(.white)
+                            .font(.system(size: 16)))
+                        .foregroundColor(.white)
+                        .padding(.leading,40)
+                        .padding(.trailing,40)
+                        
+                        Divider().frame(height: 2)
+                            .overlay(.white)
+                            .padding(.leading,40)
+                            .padding(.trailing,40)
+                            .padding(.bottom,20)
+                        
+                        Text("Email")
+                            .foregroundColor(.white)
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                            .font(.system(size: 18))
+                            .padding(.leading,40)
+                            .padding(.trailing,40)
+                            .padding(.bottom,10)
+                        
+                        TextField("", text: $username, prompt: Text("user's email")
+                            .foregroundColor(.white)
+                            .font(.system(size: 16)))
+                        .foregroundColor(.white)
+                        .padding(.leading,40)
+                        .padding(.trailing,40)
+                        
+                        Divider().frame(height: 2)
+                            .overlay(.white)
+                            .padding(.leading,40)
+                            .padding(.trailing,40)
+                            .padding(.bottom,60)
+                    }
+                    
+                    Button{
+                        
+                    } label: {
+                        Text("Save")
+                            .foregroundColor(.white)
+                            .font(.system(size: 22))
+                            .bold()
+                            .frame(maxWidth: 100, maxHeight: 24)
+                            .padding()
+                            .background(
+                                RoundedRectangle(cornerRadius: 24)
+                                    .fill(Color.black)
+                            )
+                            .padding(.horizontal)
+                    }.padding(.bottom,20)
+                    
+                    Button{
+                        
+                    } label: {
+                        Text("Log Out")
+                            .foregroundColor(.white)
+                            .font(.system(size: 22))
+                            .bold()
+                            .frame(maxWidth: 100, maxHeight: 24)
+                            .padding()
+                            .background(
+                                RoundedRectangle(cornerRadius: 24)
+                                    .fill(Color.black)
+                            )
+                            .padding(.horizontal)
+                    }.padding(.bottom,20)
+                }
             }.tabItem {
                 Image(systemName: "person").padding(.top,10)
                 Text("Profile").padding(.bottom,10)
