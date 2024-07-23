526b6d801fd660ef5eb33c7693f8b0cab64e9de2
ericadiogo <erica.monteiro.diogo@gmail.com>
2024-07-22 23:03:44 -0400

    Added achievements design.

--- PomodoroStudies/HomeView.swift
+++ PomodoroStudies/HomeView.swift
@@ -11,9 +11,9 @@ import FirebaseAuth
 struct HomeView: View {
     @State private var selectedTab = 0
     @State private var name = ""
-    @State private var userlevel = ""
+    @State private var userlevel = 0
     @State private var username = ""    //email provided by user
-    
+    @State private var points = "0"
     var body: some View {
         
         TabView(selection: $selectedTab) {
@@ -36,7 +36,7 @@ struct HomeView: View {
                             .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/,
                                    alignment: .leading)
                         
-                        Text("You are level" + userlevel + ".") //add level from database
+                        Text("You are level " + String(userlevel) + ".") //add level from database
                             .font(.system(size: 14))
                             .padding(.bottom,5)
                             .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/,
@@ -165,9 +165,9 @@ struct HomeView: View {
                                 .navigationTitle("CalendarView")
                         }
                         
-                        Text("Selected Date").font(.system(size: 24)).fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/).foregroundColor(.red) //retrieve from clicked date on calendar
+                        Text("Selected Date").font(.system(size: 24)).fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/).foregroundColor(.red).padding(.bottom,30) //retrieve from clicked date on calendar
                         
-                        
+                        Text("History List View here")
                     }
                 }
             }.tabItem {
@@ -176,68 +176,58 @@ struct HomeView: View {
             }.tag(2)
             
             // Achivements Tab
+            
             VStack{
-                HStack{
+                HStack(alignment: .center){
                     VStack{
-                        Image("PomodoroNinjaSolo-2")
+                        HStack{}.background(Color.black).frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/,maxHeight: 200)
+                        Text("Hello " + name)
+                            .font(.system(size: 50))
+                            .fontWeight(.bold).foregroundColor(.white)
+                            .padding(.top,60)
+                        Image("02")
                             .resizable()
                             .scaledToFill()
-                            .frame(width: 40, height: 64)
-                    }.padding()
-                    
+                            .frame(width: 260,height: 240)                    }
+                    //i think here we can use the ninja of the actual level of the user
+                }.frame(maxWidth: .infinity,maxHeight: 300)
+                    .background(LinearGradient(gradient: Gradient(colors:[.white,.black]), startPoint: .bottom,endPoint: .top))
+                    .padding(.top,20)
+                HStack{
                     VStack{
-                        Text("Hello, user!")
-                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-                            .font(.system(size: 18))
-                            .padding(.bottom,5)
-                            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/,
-                                   alignment: .leading)
-                        
-                        Text("You are level ?")
-                            .font(.system(size: 14))
-                            .padding(.bottom,5)
-                            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/,
-                                   alignment: .leading)
+                        HStack{
+                            Text("Level ").font(.system(size: 40)).fontWeight(.bold)
+                            Text(String(userlevel)).font(.system(size: 40))
+                                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/).foregroundColor(.red)
+                        }.padding(40)
+                        let ul = userlevel + 1
+                        Text("Points missing to Level " + String(ul))
+                        ProgressView(value: 0,total: 100).padding(20)
+                        Text("220 / 400") // add points from user
                     }.padding()
-                }.frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/,maxWidth: .infinity)
-                    .padding(.leading,20)
-                    .padding(.trailing,20)
-                
-                Text("Achievements screen")
+                }
             }.tabItem {
                 Image(systemName: "star").padding(.top,10)
                 Text("Achievements").padding(.bottom,10)
             }.tag(3)
             
             // My Studies Tab
-            VStack{
-                HStack{
-                    VStack{
-                        Image("PomodoroNinjaSolo-2")
+            ZStack{
+                VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/){
+                    HStack{
+                        Image("MyStudiesLogo")
                             .resizable()
                             .scaledToFill()
-                            .frame(width: 40, height: 64)
-                    }.padding()
-                    
-                    VStack{
-                        Text("Hello, user!")
-                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-                            .font(.system(size: 18))
-                            .padding(.bottom,5)
-                            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/,
-                                   alignment: .leading)
+                            .frame(width: 60,height: 60)
+                            .padding(10)
                         
-                        Text("You are level ?")
-                            .font(.system(size: 14))
-                            .padding(.bottom,5)
-                            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/,
-                                   alignment: .leading)
-                    }.padding()
-                }.frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/,maxWidth: .infinity)
-                    .padding(.leading,20)
-                    .padding(.trailing,20)
-                
-                Text("My studies screen")
+                        Text("My Studies")
+                            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    }.padding(30)
+                    
+                    Text("Add list view here")
+                }
             }.tabItem {
                 Image(systemName: "book").padding(.top,10)
                 Text("My Studies").padding(.bottom,10)
